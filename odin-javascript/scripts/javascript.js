14df065b9aadc8ef24d1497e1bb96d81ffe93c6d
Mbelenga <mbelengaterence@gmail.com>
2024-12-12 23:56:38 +0300

    javascript basics

---
+++ odin-javascript/scripts/javascript.js
@@ -0,0 +1,11 @@
+let name = "Koki";
+let surname = "Mutungi";
+let age = 38;
+
+console.log(name);
+console.log(surname);
+console.log(age); // outputs 38 to the console
+
+age = 45;
+
+console.log(age); // outputs 45 to the console
