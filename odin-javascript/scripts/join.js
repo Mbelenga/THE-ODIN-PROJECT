98757d045283c9f039e5d1cfa536a6c0ed16364a
Mbelenga <mbelengaterence@gmail.com>
2024-12-13 01:21:04 +0300

    Updated

---
+++ odin-javascript/scripts/join.js
@@ -0,0 +1,5 @@
+// This is called concatenation - the process of joining strings together.
+const one = "Hello, ";
+const two = "how are you?";
+const joined = `${one}${two}`;
+console.log(joined); // "Hello, how are you?"
